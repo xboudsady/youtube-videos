ee3cc49e3c73f2928e0af439b3eb7090b30f07ef
xboudsady <boudsadyx@gmail.com>
2019-08-10 23:22:19 +0200

    Updated onTermSubmit() function from App class, to an async, await function. Create state with empty array to hold our data to be fetch.

--- src/components/App.js
+++ src/components/App.js
@@ -3,13 +3,20 @@ import SearchBar from './SearchBar';
 import youtube from '../api/youtube';
 
 class App extends React.Component {
+  // create a state with empty array to hold our list of data when fetch is called
+  state = { 
+    videos: []
+  }
+  
   // Submit input field, trigger YouTube API data fetch
-  onTermSubmit = term => {
-    youtube.get('/search', {
+  onTermSubmit = async term => {
+    const response = await youtube.get('/search', {
       params: {
         q: term
       }
     });
+
+    this.setState({ videos: response.data.items });
   }
   
   render() {
@@ -17,6 +24,7 @@ class App extends React.Component {
       <div className="ui container">
         {/* Calls the method to fetch API data */}
         <SearchBar onFormSubmit={ this.onTermSubmit } />
+        I have { this.state.videos.length } videos.
       </div>
     )
   }
