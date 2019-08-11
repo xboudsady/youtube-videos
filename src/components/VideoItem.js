779ad1e58d3799578a586b4323a55626e60a9c18
xboudsady <boudsadyx@gmail.com>
2019-08-11 11:31:38 +0200

    Updated <VideoItem /> component to render fetched data onscreen. Render thumbnail image, and video title.

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
-const VideoItem = (props) => {
-  return <div>Video Item</div>
+// props as an argument is passed from parent <VideoList /> component, from the fetch data API
+const VideoItem = ({ video }) => {
+  return <div>
+      <img src={ video.snippet.thumbnails.medium.url } />
+      { video.snippet.title }
+    </div>
 };
 
 export default VideoItem;
--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -4,7 +4,7 @@ import VideoItem from './VideoItem';
 const VideoList = ({ videos }) => {
   // Array passed rom the mapped component, passd down as an argument
   const renderedList = videos.map((video) => {
-    return <VideoItem />;
+    return <VideoItem video={ video } />;
   });
   
   return <div>{ renderedList }</div> 
