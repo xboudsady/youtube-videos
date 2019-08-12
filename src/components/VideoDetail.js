4767266c5be88692cb0765ad54a420baa33d0ed1
xboudsady <boudsadyx@gmail.com>
2019-08-12 03:44:43 +0200

    Updated the <VideoDetail /> component, additional styling via semantics-ui.

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -1,11 +1,21 @@
 import React from 'react';
 
+// The video 'object' passed as argumen here is the data from the API request
+// Use use deconstructure, to avoid having to stype prop.video
 const VideoDetail = ({ video }) => {
   // Check if video state is null, which is true based on initial state from parent component <App />
   if (!video) {
     return <div>Loading...</div>
   }
-  return <div>{ video.snippet.title }</div>
+
+  return (
+    <div>
+      <div className="ui segment">
+        <h4 className="ui header">{ video.snippet.title }</h4>
+      </div>
+      <p>{ video.snippet.description }</p>
+    </div>
+  );
 };
 
 export default VideoDetail;
